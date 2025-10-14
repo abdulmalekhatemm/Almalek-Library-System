21baeb3e287b8b499105cebc052b4b7cbce84e3f
abdulmalek qasem hatem <alhomedyabboud@gmail.com>
2025-10-14 23:28:19 +0300

    add some commit

--- about.php
+++ about.php
@@ -9,7 +9,7 @@ include 'info.php';
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="css.css">
-<title>Document</title>
+<title>About Library </title>
 </head>
 <body>
 
