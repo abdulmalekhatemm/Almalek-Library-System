9ed11e2e92f604b6576193435f8bcf2083986ffa
abdulmalek qasem hatem <alhomedyabboud@gmail.com>
2025-10-15 17:23:45 +0300

    add athore commit

--- Plane.php
+++ Plane.php
@@ -7,7 +7,7 @@ include 'info.php';
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+<title>Plan Page</title>
 </head>
 <style>
 </style>
--- US.php
+++ US.php
@@ -9,7 +9,7 @@ include 'css.css';
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="css.css">
-<title>Document</title>
+<title>US</title>
 </head>
 <body>
 <center>
